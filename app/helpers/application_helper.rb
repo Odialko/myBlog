aa7e08ba132ce497bf837537eb107c929ebc7da2
odialko <viktor1532@gmail.com>
2018-11-18 00:36:44 +0200

    generate controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def icon(name)
+    content_tag(:i, '', class: "fa fa-#{name} mr-3", 'aria-hidden': "true")
+  end
+
+  def format_date_time(date_time, delimiter = '.')
+    date_time.try(:strftime, ("%d#{delimiter}%m#{delimiter}%Y - %H:%M")) unless date_time.blank?
+  end
 end
