1f77917d1124823446e854b6e2df00bea4faa73f
odialko <viktor1532@gmail.com>
2018-11-19 17:17:53 +0200

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.assets.initialize_on_precompile = true
+  config.action_controller.consider_all_requests_local = false
 end
