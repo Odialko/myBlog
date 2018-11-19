36f440504f720b2506ddf9629bef05f7439b0fda
odialko <viktor1532@gmail.com>
2018-11-19 21:55:08 +0200

    titelize

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,12 @@
 class Comment < ActiveRecord::Base
 
+  before_save :titleize_attr
+
+  belongs_to :commentable, :polymorphic => true
   validates :content, presence: true
   validates :author, presence: true
 
-  belongs_to :commentable, :polymorphic => true
+  def titleize_attr
+    self.author = author.titleize
+  end
 end
