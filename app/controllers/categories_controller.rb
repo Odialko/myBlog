5330475486b54588e90183875581d4bdaf7d4b27
odialko <viktor1532@gmail.com>
2018-11-23 10:12:33 +0200

    sintaxis

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :set_category, only: %i[show edit update destroy]
 
   def index
     @categories = Category.order(created_at: :desc).page(params[:page]).per(PER_PAGE_5)
